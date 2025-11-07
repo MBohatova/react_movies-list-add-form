6a9f336fe5aa1c86ababa3016e17a318a5743ea9
Mariia Bohatova <maria.bogatova.01@gmail.com>
2025-11-07 19:41:03 +0200

    correct solution 2

--- src/App.tsx
+++ src/App.tsx
@@ -22,7 +22,7 @@ export const App = () => {
   return (
     <div className="page">
       <div className="page-content">
-        <MoviesList movies={moviesFromServer} />
+        <MoviesList movies={movies} />
       </div>
       <div className="sidebar">
         <NewMovie onAdd={handleAddMovie} />
